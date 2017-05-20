a0f17fe159b8a65bb5c892279d992a76237ff694
vwillcox <talking@talktech.info>
2017-05-20 05:42:50 +0100

    Add Zones

--- lights.py
+++ lights.py
@@ -7,38 +7,45 @@ import ledcontroller
 def main(argv):
    cmd = ''
    value = ''
-   led = ledcontroller.LedController("192.168.0.139")
+   zone = ''
+   led = ledcontroller.LedController("192.168.0.150")
    try:
-      opts, args = getopt.getopt(argv,"hc:v:",["cmd=","value="])
+      opts, args = getopt.getopt(argv,"hc:v:z:",["cmd=","value=","zone="])
    except getopt.GetoptError:
-      print 'lights.py -c <command> -v <value>'
+      print 'lights.py -c <command> -v <value> -z <value>'
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'lights.py -c <command> -v <value>'
+         print 'lights.py -c <command> -v <value> -z <value>'
          sys.exit()
       elif opt in ("-c", "--cmd"):
          cmd = arg
       elif opt in ("-v", "--value"):
          value = arg
+      elif opt in ("-z", "--zone"):
+         try:
+            zone = int(arg)
+         except ValueError:
+            zone = None
    print 'Command is :', cmd
    print 'Value is :', value
+   print 'Zone is :', zone
    led.set_group_type(1,'rgbw')
    if cmd == 'on' and value == 'white':
-       led.on()
+       led.on(zone)
        led.set_color('white')
    if cmd == 'on' and value == 'red':
-       led.on()
+       led.on(zone)
        led.set_color('red')
    if cmd == 'on' and value == 'green':
-       led.on()
+       led.on(zone)
        led.set_color('green')
    if cmd == 'on' and value == 'blue':
-       led.on()
+       led.on(zone)
        led.set_color('royal_blue')
    if cmd == 'off':
        led.set_color('white')
-       led.off()
+       led.off(zone)
    if cmd == 'dim':
        led.set_brightness(int(value))       
 
